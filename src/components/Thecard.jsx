6fab1fb12590f9b22ca28e77aa1418255f1caaf3
ansh <ansh.jict22@sot.pdpu.ac.in>
2025-04-08 19:51:02 +0530

    Delete src/components/Thecard.jsx

--- src/components/Thecard.jsx
+++
@@ -1,7 +0,0 @@
-const TheCard = ({ content, name, title, img }) => (
-  <div className="flex justify-between flex-col px-2 py-2 rounded-[30px]  max-w-[1207px] md:mr-10 sm:mr-2 mr-0 my-2 feedback-card">
-    <img src={img} alt={name} className="w-full max-h-[400px] rounded-[30px]" />
-  </div>
-);
-
-export default TheCard;
