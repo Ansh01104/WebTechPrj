57346cdfb549d41834cc76a9ecde8fe8816e2d83
omrptl <143069979+omrptl@users.noreply.github.com>
2025-04-16 09:44:14 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,73 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import download from "./download.png";
+import downloadx from "./downloadx.png";
+import videoframe_10835 from "./videoframe_10835.jpeg";
+import shit from "./shit.mp4";
+import shitmoves from "./shitmoves.mp4";
+import banner_finalpro from "./banner_finalpro.mp4";
+
+import full_logo from "./full_logo.png";
+ 
+ 
+import imagesx from "./donloa.jpg";
+export {
+  airbnb,
+  download,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+  downloadx,
+  videoframe_10835,
+  shit,
+  shitmoves,
+  banner_finalpro,
+  full_logo, 
+ 
+  imagesx,
+  
+};
