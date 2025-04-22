4c33da9bccf0b310543073a13df6c37e50014cbb
Saumya Shah <saumya.sict22@sot.pdpu.ac.in>
2025-04-23 01:15:41 +0530

    Some Minor Changes

---
+++ tailwind.config.cjs
@@ -0,0 +1,68 @@
+
+ 
+ 
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,jsx}" // Includes all JS/JSX files inside src
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+        sexy: 'hsla(192, 100%, 90%, 0.34)',
+        gold__gradient: "rgba(255, 215, 0, 0.1)",
+       thetrans: "hsla(0, 0%, 98%, 0.165)",
+    },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+    function ({
+      matchUtilities,
+      theme
+    }) {
+      matchUtilities({
+        "bg-dot-thick": (value) => ({
+          backgroundImage: `url("${svgToDataUri(
+            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+          )}")`,
+        }),
+      }, { values: flattenColorPalette(theme("backgroundColor")), type: "color" });
+    },
+  ],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({
+  addBase,
+  theme
+}) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
+}
