96ae5f32e6e473f784f11d4da409aaa6199e0bd4
servalabs <166895751+servalabs@users.noreply.github.com>
2025-04-23 08:19:02 +0530

    modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
   <section className={`${styles.flexCenter} sm:py-2 py-5 flex-col`}>
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-2 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-base text-center text-[13px] leading-[27px] text-white">
-        Copyright Ⓒ 2024 BuddingTECH. All Rights Reserved.
+        Copyright Ⓒ 2025 ServaLabs. All Rights Reserved.
       </p>
 
       
